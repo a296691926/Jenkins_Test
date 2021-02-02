2e94f01ed45183864dda82a7aaf2e5cc47bc8bed
“a296691926” <296691926@qq.com>
2021-02-02 10:55:39 +0800

    测试自动集成测试2/2

--- fixture_test_21.py
+++ fixture_test_21.py
@@ -35,10 +35,6 @@ class Test_oo:
             allure.attach(a, "不等于3")
             assert a != 3
 
-    @pytest.allure.step("我是步骤五")
-    def test_oo5(self, a):
-        allure.attach(a, "不等于3")
-        assert a != 3
 
 
 if __name__ == '__main__':
